22e0a6f3fddb47295b6efdcfd12aa0d3d14be878
Terry Wambui <shikuwambui@gmail.com>
2023-07-11 18:16:33 +0300

    create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,25 @@
 
 
 from uuid import uuid4
-from datetime import datetime
+from datetime import date, datetime
 
 
 class BaseModel:
     """The super class"""
-    def __init__(self):
-        """Initializing the base model"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initializing the base model"""    
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """sets the print behaviour of the base model"""
@@ -26,7 +35,7 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all key/values of __dict__"""
         new_dict = self.__dict__.copy()
-        new_dict["created_at"] = self.created_at.isoformat
-        new_dict["updated_at"] = self.updated_at.isoformat
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
