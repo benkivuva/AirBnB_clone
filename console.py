d232656661f95fd387a28fdb76d7097364e2003b
Benkivuva <benkivuvak5@gmail.com>
2023-07-12 10:26:18 +0300

    Console 0.0.1

--- console.py
+++ console.py
@@ -3,3 +3,31 @@
 """Defines the HBnB console"""
 
 import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """
+       Quit command to exit the program
+
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
