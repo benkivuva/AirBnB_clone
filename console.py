efa3e73efd5ae10340c181c325ff95ecbb909fe0
Benkivuva <benkivuvak5@gmail.com>
2023-07-12 11:50:16 +0300

    add create and show

--- console.py
+++ console.py
@@ -3,22 +3,25 @@
 """Defines the HBnB console"""
 
 import cmd
+import json
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
+    """ hbnb shell """
     prompt = '(hbnb) '
+    clslist = {'BaseModel': BaseModel}
 
     def do_quit(self, arg):
         """
-       Quit command to exit the program
-
+        Quit command to exit the program
         """
         return True
 
     def do_EOF(self, arg):
         """
         EOF command to exit the program
-
         """
         return True
 
@@ -27,7 +30,41 @@ class HBNBCommand(cmd.Cmd):
         Do nothing when an empty line is entered.
         """
         pass
+    
+    def do_create(self, clsname=None):
+        """Creates a new instance of BaseModel, saves it and prints the id
+        """
+        if not clsname:
+            print('** class name missing **')
+        elif not self.clslist.get(clsname):
+            print('** class doesn\'t exist **')
+        else:
+            obj = self.clslist[clsname]()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, arg):
+        """Show instance based on id"""
+        clsname, objid = None, None
+        args = arg.split(' ')
+        if len(args) > 0:
+            clsname = args[0]
+        if len(args) > 1:
+            objid = args[1]
+        if not clsname:
+            print('** class name missing **')
+        elif not objid:
+            print('** instance id missing **')
+        elif not self.clslist.get(clsname):
+            print("** class doesn't exist **")
+        else:
+            k = clsname + "." + objid;
+            obj = models.storage.all().get(k)
+            if not obj:
+                print('** no instance found **')
+            else:
+                print(obj)
 
 
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    if __name__ == "__main__":
+     HBNBCommand().cmdloop()     
