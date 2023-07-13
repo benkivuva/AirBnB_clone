b6c072a0ddc97718804a69b185deb605ce9365a1
Terry Wambui <shikuwambui@gmail.com>
2023-07-13 15:51:14 +0300

    fixing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@
 
 import json
 from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 class FileStorage:
@@ -48,15 +54,29 @@ class FileStorage:
     def reload(self):
         """
         Deserializes the JSON file and updates the objects dictionary.
-        If the JSON file (__file_path) exists, it reads file and loads objects
+        If the JSON file (__file_path) exists it reads the file and loads objects.
         If the file doesn't exist, it does nothing.
         """
+        clslist = {
+            'BaseModel': BaseModel,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review,
+            'User': User
+        }
 
         try:
-            with open(self.__file_path, "r", encoding="UTF-8") as f:
-                obj = json.loads(f.read())
-            for k, v in obj.items():
-                class_name = k.split('.')[0]
-                self.__objects[k] = eval(class_name)(**v)
-        except BaseException:
+            with open(self.__file_path, "r") as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    self.__objects[key] = globals()[class_name](**value)
+        except FileNotFoundError:
             pass
+
+
+if __name__ == "__main__":
+    storage = FileStorage()
+    storage.reload()
